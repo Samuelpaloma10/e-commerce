286517d27daca1e68c26ebde557b59627d66f761
ESNEYDER PALOMA RIVERA <samupalo3@gmail.com>
2024-08-29 18:55:10 -0500

    dosss

---
+++ index.js
@@ -0,0 +1,9 @@
+let hola = "holaaaa";
+console.log('hola');
+
+const title = document.querySelector(".card-title");
+const img = document.querySelector(".card-img-top");
+const text =  document.querySelector(".card-text");
+title.innerText = "Holaaaa";
+img.src='https://www.civitatis.com/blog/wp-content/uploads/2024/04/paisajes-interlaken-suiza.jpg';
+text.innerText = "vamossssssss";
